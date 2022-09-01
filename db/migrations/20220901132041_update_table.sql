4a3b6e0ac777688b15a51f602468ce22f29719db
Mark Kim <mark340@naver.com>
2022-09-01 22:48:54 +0900

    [ADD]: 테이블 컬럼추가/ 유저-생년월일,성별 등

---
+++ db/migrations/20220901132041_update_table.sql
@@ -0,0 +1,9 @@
+-- migrate:up
+ALTER TABLE users ADD birth int NOT NULL;
+ALTER TABLE users ADD sex varchar(50) NOT NULL;
+ALTER TABLE point ADD created_at timestamp DEFAULT (CURRENT_TIMESTAMP);
+ALTER TABLE point ADD description varchar(50) NULL;
+ALTER TABLE bundle CHANGE bundle_name bundle_option varchar(50) ;
+
+-- migrate:down
+
